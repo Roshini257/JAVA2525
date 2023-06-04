be925583e746a11132f829f8d752af9eab6699fd
Roshini Racha <roshiniracha1997@gmail.com>
2023-06-04 15:49:05 -0500

    updated

---
+++ Week1/src/Main.java
@@ -0,0 +1,37 @@
+package Week1.src;
+
+import java.util.*;
+import java.lang.System.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Student s = new Student();
+        System.out.println("Enter-name :");
+        Scanner sc= new Scanner(System.in);
+        String n= sc.nextLine();
+        s.setName(n);
+        //   System.out.println("Name is "+s.name);
+        String name = s.getName();
+        System.out.println("Name: " + name);
+
+      CSE c1 = new CSE();
+      int n1 = c1.getDepartmentSize();
+        System.out.println("Size:"+n1);
+
+        Product p = new Product();
+        p.setPrice(12);
+        double d = p.getPrice(4);
+        System.out.println("price: " + d);
+
+        Vehicle v= new Vehicle();
+        Car c = new Car();
+        Vehicle v2 = new Car();
+
+        v.color();
+        c.color();
+        v2.color();
+
+
+
+}}

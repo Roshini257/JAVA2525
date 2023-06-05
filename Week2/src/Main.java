c84b93732cdabb0bb33c4d17196bd5ec4d797337
Roshini Racha <roshiniracha1997@gmail.com>
2023-06-04 23:35:15 -0500

    Week2 added

---
+++ Week2/src/Main.java
@@ -0,0 +1,52 @@
+package Week2.src;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Interface
+
+        Child c =new Child();
+        c.setName("Example of Interfaces");
+        String name = c.getParent1name();
+        System.out.println("Name: " + name);
+
+//        Constructor
+
+        kid k = new kid();
+        kid k1= new kid (4);
+        kid k2= new kid(33,"Example of Constructor");
+
+
+
+//        System.out.println("Default constructor--"+k.age);
+        System.out.println("Constructor with single parameter--"+k1.age);
+        System.out.println(k1.name);
+        System.out.println(k2.age);
+        System.out.println(k2.name);
+
+//        static
+        Static s = new Static();
+        System.out.println("Static variable value is "+Static.age());
+
+
+
+//        Final
+
+        Final f = new Final();
+        f.finalstring();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}

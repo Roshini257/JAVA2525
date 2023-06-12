a3aada762f1cb8415695794be7c23f70782f2fe4
Roshini Racha <roshiniracha1997@gmail.com>
2023-06-12 15:08:57 -0500

    Intro

--- Week2/src/ReverseArray.java
+++ Week2/src/ReverseArray.java
@@ -1,13 +1,21 @@
 package Week2.src;
 
+import java.util.Arrays;
+
 public class ReverseArray {
     int array[] = {1, 2, 3, 4, 5};
+    char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+
     int[] dest_array = new int[5];
 
+    String helloString = new String(helloArray);
+
     int n = array.length;
-    int d= dest_array.length;
+//    int d= dest_array.length;
 
     public void arrayReverse() {
+        System.out.println(helloString);
+
 
         System.out.println("Original array: ");
         for (int k = 0; k < n; k++) {

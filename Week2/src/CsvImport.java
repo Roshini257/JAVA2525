fe940653d94bad430739bc1e847609bd96668e9c
Roshini Racha <roshiniracha1997@gmail.com>
2023-06-12 00:41:21 -0500

    ss

---
+++ Week2/src/CsvImport.java
@@ -0,0 +1,35 @@
+package Week2.src;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import static java.lang.Integer.parseInt;
+import static java.util.Arrays.*;
+
+public class CsvImport {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File f = new File("/Users/roshiniracha/Documents/Student.csv");
+        Scanner s = new Scanner(f);
+        String line;
+
+        Student1[] stu = new Student1[10];
+        for (int i = 0; s.hasNextLine(); i++) {
+            line = s.nextLine();
+            String[] str = line.split(",");
+            String firstName = str[0];
+            String lastName = str[1];
+            int gpa = Integer.parseInt(str[2]);
+            stu[i] = new Student1(firstName, lastName, gpa);
+
+        }
+
+        sort(stu);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(stu[i]);
+        }
+
+    }
+}

299c5d25a73e3fd3121cbadba6396b5c47ed4834
Roshini Racha <roshiniracha1997@gmail.com>
2023-06-19 15:27:06 -0500

    update

--- Week3/StringCapital.java
+++ Week3/StringCapital.java
@@ -6,15 +6,12 @@ public class StringCapital{
     public static void main(String[] args) {
         String[] s = {"food", "movie", "novel", "cooking", "apple"};
 
-        // Capitalize the first letter of each string
         String[] cap = Arrays.stream(s)
                 .map(s1 -> s1.substring(0, 1).toUpperCase() + s1.substring(1))
                 .toArray(String[]::new);
 
-        // Sort the capitalized strings in alphabetical order
         Arrays.sort(cap);
 
-        // Print the sorted and capitalized strings
         Arrays.stream(cap)
                 .forEach(System.out::println);
     }

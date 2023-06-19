95a8396d96b9ca467d0c55d52d967c74b055aea2
Roshini Racha <roshiniracha1997@gmail.com>
2023-06-19 14:39:10 -0500

    Strings and streams

---
+++ Week3/StringCapital.java
@@ -0,0 +1,21 @@
+package Week3;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class StringCapital{
+    public static void main(String[] args) {
+        String[] s = {"Food", "Movie", "Novel", "Cooking", "Apple"};
+
+        // Capitalize the first letter of each string
+        String[] cap = Arrays.stream(s)
+                .map(s1 -> s1.substring(0, 1).toUpperCase() + s1.substring(1))
+                .toArray(String[]::new);
+
+        // Sort the capitalized strings in alphabetical order
+        Arrays.sort(cap);
+
+        // Print the sorted and capitalized strings
+        Arrays.stream(cap)
+                .forEach(System.out::println);
+    }
+}
---
+++ Week3/stringvowel.java
@@ -0,0 +1,24 @@
+package Week3;
+
+import java.util.Arrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class stringvowel {
+    public static void main(String[] args) {
+        String[] strings = {"apple", "banana", "cat", "dog", "elephant"};
+
+//        List<String> vowelStrings = Arrays.asList(strings);
+
+        Arrays.stream(strings)
+                .filter(s -> s.matches(".[aeiouAEIOU]."))
+                .forEach(s -> System.out.println(s + " has " + countVowels(s) + " vowels"));
+    }
+
+
+    private static int countVowels(String str) {
+        return str.replaceAll("(?i)[^aeiouAEIOU]", "").length();
+    }
+
+}
